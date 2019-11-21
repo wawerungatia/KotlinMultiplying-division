2e87fde9709d4977eea4dd67f8020c582cf8fc61
wawerungatia <joseph.waweru@riarauniversity.ac.ke>
2019-11-21 18:40:39 +0300

    Feat:multiplication Divisipn and funtions

---
+++ src/Division.kt
@@ -0,0 +1,16 @@
+fun main (args:Array<String>){
+    val one:Double =100.0;
+    val two:Double=39.0;
+    val divid:Double =one/two
+    println(divid)
+    floatFuntion()
+}
+fun floatFuntion(){
+    val a:Float=10.8436543f
+    val b:Float=653.545432f
+    val divide:Float=a%b
+    println("the modulus of b and is:"+divide)
+
+
+
+}
